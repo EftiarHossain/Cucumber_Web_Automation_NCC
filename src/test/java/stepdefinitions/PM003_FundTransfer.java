bd22bfbb577f229544dfd2bb888c38a90c462697
EftiarHossain <mdnaim644@gmail.com>
2025-03-04 06:57:15 +0600

    Added FundTransfer

---
+++ src/test/java/stepdefinitions/PM003_FundTransfer.java
@@ -0,0 +1,19 @@
+package stepdefinitions;
+
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import objectRepository.PG003_DashboardOR;
+import utils.Base;
+import utils.Operations;
+
+public class PM003_FundTransfer extends Base {
+
+    @Then("I navigate to the Own Account Fund Transfer")
+    public void iNavigateToTheOwnAccountFundTransfer() {
+
+    }
+
+
+
+
+}
