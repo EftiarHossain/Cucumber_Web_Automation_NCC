4f7e2b6420d81fea39d574b0fd5f46b438da9343
EftiarHossain <mdnaim644@gmail.com>
2025-03-08 13:28:50 +0600

    Upload files

--- src/test/java/resources/features/TC003_Dashboard.feature
+++ src/test/java/resources/features/TC003_Dashboard.feature
@@ -3,11 +3,11 @@ Feature: Dashboard
   Scenario: Validate Dashboard, Account Details, Statement Download, DPS, FDR, Loan & Card Details
     When I navigate to my dashboard
     Then I can see my Accounts
-    When I click on any Account
-    Then I can see mini details of my Account
-    When I click on View Statement from the mini details
-    Then I can see the Statement search by Account and able to download statements
-    When I navigate to my dashboard
+#    When I click on any Account
+#    Then I can see mini details of my Account
+#    When I click on View Statement from the mini details
+#    Then I can see the Statement search by Account and able to download statements
+#    When I navigate to my dashboard
     Then I can click on the Card Tab to see my Credit Cards
     When I click on any Card
     Then I can see mini details of the card
--- src/test/java/utils/Base.java
+++ src/test/java/utils/Base.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
+import java.io.File;
 import java.nio.file.Paths;
 import java.time.Duration;
 import java.util.HashMap;
@@ -25,10 +26,14 @@ public class Base {
         prefs.put("profile.default_content_settings.popups", 0);
         prefs.put("profile.content_settings.exceptions.automatic_downloads.*.setting", 1);
 
-        prefs.put("download.default_directory", System.getProperty("user.dir") + "/downloads");  // Relative to workspace
-        prefs.put("download.prompt_for_download", false);
+        String projectPath = System.getProperty("user.dir");  // Root of the project
+        String downloadPath = projectPath + File.separator + "downloads";  // Inside project folder        prefs.put("download.prompt_for_download", false);
         prefs.put("download.directory_upgrade", true);
         prefs.put("safebrowsing.enabled", true);
+        File downloadDir = new File(downloadPath);
+        if (!downloadDir.exists()) {
+            downloadDir.mkdirs();  // Create directory if it doesn't exist
+        }
 
         options.setExperimentalOption("prefs", prefs);
         WebDriverManager.chromedriver().setup();
